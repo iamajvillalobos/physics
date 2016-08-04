d67633aba89364ba6b2386a28da799956f0562e2
Arman Jon Villalobos <me@armanjon.com>
2016-08-04 22:18:23 +0800

    added Converter module and tests

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,20 +1,41 @@
-defmodule Physics.Planet do
-  defstruct [
-    name: "Earth",
-    radius_m: 6.371e6,
-    mass_kg: 5.97e24
-  ]
-
-  def escape_velocity(planet) do
-    g = 6.67e-11
-    gmr = 2 * g * planet.mass_kg/planet.radius_m
-    vms = :math.sqrt(gmr)
-    vkms = vms/1000
-    Float.ceil(vkms, 1)
+defmodule Physics.Rocketry do
+  def escape_velocity(:earth) do
+    %{mass: 5.972e24, radius: 6.371e6}
+      |> escape_velocity
   end
+
+  def escape_velocity(planet) when is_map(planet) do
+    planet
+      |> calculate_escape
+      |> Converter.to_km
+      |> Converter.to_nearest_tenth
+  end
+
+  defp calculate_escape(%{mass: mass, radius: radius}) do
+    newtons_constant = 6.67e-11
+    2 * newtons_constant * mass / radius
+      |> :math.sqrt
+  end
+
+
 end
 
-v = %Planet{} |> Physics.Planet.escape_velocity
+defmodule Converter do
+  def to_km(velocity) do
+    velocity / 1000
+  end
 
-defmodule Physics.Rocketry do
+  def to_nearest_tenth(val) do
+    Float.ceil(val, 1)
+  end
+
+  def to_light_seconds({:miles, miles}) do
+    (miles * 5.36819e-6) |> round_down
+  end
+
+  def to_light_seconds({:meters, meters}) do
+    (meters * 3.335638620368e-9) |> round_down
+  end
+
+  def round_down(val) when is_float(val), do: trunc(val)
 end
